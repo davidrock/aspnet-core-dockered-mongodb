538636f7ba15e9a1c13d54a7f47665906566bf11
david.rocha <davidrocha@outlook.com>
2018-07-25 18:05:03 -0300

    initial commit

---
+++ Database/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class Address
+    {
+        public Guid Guid { get; private set; }
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string ZipCode { get; private set; }
+    }
+}
---
+++ Database/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Models
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public Address Address { get; private set; }
+
+        public User(string name, string document, Address address)
+        {
+            Id = Guid.NewGuid();
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Document = document ?? throw new ArgumentNullException(nameof(document));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+
+        public void ChangeName(string newName)
+        {
+            if (string.IsNullOrEmpty(newName) || string.IsNullOrEmpty(newName))
+            {
+                throw new Exception("Valor não pode ser nulo");
+            }
+
+            this.Name = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(newName.Trim().ToLower());
+            
+        }
+
+        public void AddName(string newName)
+        {
+            if (string.IsNullOrEmpty(newName) || string.IsNullOrEmpty(newName))
+            {
+                throw new Exception("Valor não pode ser nulo");
+            }
+
+            this.Name = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(newName.Trim().ToLower());
+
+        }
+
+    }
+}
